bcd30ebe08e28fa89a1d4f230f596d2e797fbf5b
Nuno Sousa <nuno.sousa@customer.io>
2023-08-02 11:42:16 +0100

    Remove userId requirement from group calls

--- group.go
+++ group.go
@@ -28,13 +28,5 @@ func (msg Group) Validate() error {
 		}
 	}
 
-	if len(msg.UserId) == 0 && len(msg.AnonymousId) == 0 {
-		return FieldError{
-			Type:  "analytics.Group",
-			Name:  "UserId",
-			Value: msg.UserId,
-		}
-	}
-
 	return nil
 }
